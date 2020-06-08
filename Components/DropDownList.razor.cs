417351b2089b133c8b5903fcca49f8b4adb4a7a4
Daniel <Daniel@DANIEL-NUC>
2020-06-07 21:56:17 -0500

    Added DropDownList component

---
+++ Components/DropDownList.razor.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlazorTabs.Components
+{
+    public partial class DropDownList
+    {
+        private ElementReference m_btnToggleRef;
+        private ElementReference m_divDropdownMenuRef;
+        private DotNetObjectReference<DropDownList> m_componentRef;
+
+        private bool m_isVisible = false;
+
+        private string SettingsDropdownCssClass => m_isVisible ? "show" : null;
+
+        [Parameter]
+        public IEnumerable<string> Items { get; set; }
+
+        [Parameter]
+        public EventCallback<string> ItemSelected { get; set; }
+
+        [Parameter]
+        public string ButtonImageClass { get; set; }
+
+        [Parameter]
+        public string ButtonText { get; set; }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                m_componentRef = DotNetObjectReference.Create(this);
+            }
+        }
+
+        private async Task ToggleVisible()
+        {
+            m_isVisible = !m_isVisible;
+            await SetVisible();
+        }
+
+        private async Task SetVisible()
+        {
+            if (m_isVisible)
+            {
+                await JSRuntime.InvokeVoidAsync("blazorTabs.registerDropdownComponent", m_btnToggleRef, m_divDropdownMenuRef, m_componentRef);
+            }
+            else
+            {
+                await JSRuntime.InvokeVoidAsync("blazorTabs.unregisterDropdownComponent", m_componentRef);
+            }
+        }
+
+        private void OnClick(string option)
+        {
+            //AppState.SetRoutingType((RoutingType)(Enum.Parse(typeof(RoutingType), option)));
+            m_isVisible = false;
+            ItemSelected.InvokeAsync(option);
+        }
+
+        [JSInvokable]
+        public async Task CloseDropdown()
+        {
+            m_isVisible = false;
+            await SetVisible();
+            StateHasChanged();
+        }
+
+        public void Dispose()
+        {
+            m_componentRef?.Dispose();
+        }
+    }
+}
